f62ace2c3f459b5061b93d4d25eceb9ad7bd6f15
loretta-dhahabu <loreydjefwa@gmail.com>
2022-07-14 17:35:48 +0300

    Done.

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,10 +5,14 @@ function Login() {
     event.preventDefault();
     console.log("I submit");
   }
-
+ 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="username" placeholder="Enter username..." />
+      <input
+        type="text"
+        name="username"
+        placeholder="Enter username..."
+      />
       <input type="password" name="password" placeholder="Enter password..." />
       <button>Login</button>
     </form>
